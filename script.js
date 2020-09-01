dfa012895e4d9b05f36c36b64bd9b4872af439af
jesper wouters <jesperwouters@hotmail.com>
2020-09-01 22:17:28 +0200

    final commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// promp to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    }catch(error) {
+        // this where we catch error
+        console.log('whoooopppss, error here:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+
+// on load
+selectMediaStream();
